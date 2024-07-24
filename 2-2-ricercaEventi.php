261c2e7ef46d5d98c46022e00ea831577c62a10b
paolo-mangraviti-itimonaco-it <80854427+paolo-mangraviti-itimonaco-it@users.noreply.github.com>
2024-07-24 16:18:11 +0200

    Update 2-2-ricercaEventi.php

--- 2-2-ricercaEventi.php
+++ 2-2-ricercaEventi.php
@@ -6,45 +6,39 @@
   <body>
     <h1>Risultato Ricerca</h1>
     <?php
-        $titolo=(isset(is_string($_POST['titoloEvento']))?$_POST['titoloEvento']:"");
-        $localita=(isset(is_string($_POST['localita']))?$_POST['localita']:"");
-        $tipo="%".(isset(is_string($_POST['r1']))?$_POST['r1']:"")."%";
-        $accesso="%".(isset(is_string($_POST['r3']))?$_POST['r3']:"")."%";
-            
-        $serverName = "localhost";
-        $IPserver = "127.0.1";
-        $username = "applicazioneWeb";
-        $password = "123456_Web&&";
-        $db = "listaeventi";
+		include "0-0-dbConfig.php";
+		
+        $titolo=((is_string($_POST['titoloEvento']))?$_POST['titoloEvento']:"")."%";
+        $localita=((is_string($_POST['loc']))?$_POST['loc']:"")."%";
+        $tipo="%".((is_string($_POST['r1']))?$_POST['r1']:"")."%";
+        $accesso="%".((is_string($_POST['r3']))?$_POST['r3']:"")."%";
 
         // Stabilisce la connessione al DBMS remoto
         $connessione = mysqli_connect($serverName, $username, $password, $db);
 
         // Verifica la connessione
         if (!$connessione) { die("Errore connessione");	}
-
-
+		
         // Predisposizione della query di modifica
         $istruzioneSQL = mysqli_prepare($connessione,"SELECT * FROM eventi WHERE titolo LIKE ? AND localita LIKE ? AND tipo LIKE ? AND accesso LIKE ?");
 
         mysqli_stmt_bind_param($istruzioneSQL,"ssss",$titolo,$localita,$tipo, $accesso);
-        $risultato=mysqli_stmt_execute($istruzioneSQL);
-
-        echo("<p>".$istruzioneSQL."</p>");
+		
+		mysqli_stmt_execute($istruzioneSQL);
+        
+		$risultato = mysqli_stmt_get_result($istruzioneSQL);
 
         // Visualizzazione del risultato della query
         if (mysqli_num_rows($risultato) > 0) 
         {
             echo("<ul>");
-            // output data of each row
             while($riga = mysqli_fetch_assoc($risultato)) 
             {
             echo "<li>id: " . $riga["id"]. " - Titolo: " . $riga["titolo"]. " - Descrizione: " . $riga["descrizione"] . " - Latitudine: " . $riga["lat"] ."</li>";
             }
             echo("</ul>");
         } 
-        else 
-            { echo "Nessun risultato"; }
+        else { echo "Nessun risultato"; }
 
         mysqli_close($connessione);
     ?> 	
