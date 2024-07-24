24bd81fe408307074b5c127a4208c9fefb24971b
paolo-mangraviti-itimonaco-it <80854427+paolo-mangraviti-itimonaco-it@users.noreply.github.com>
2024-07-24 16:19:10 +0200

    Add files via upload

---
+++ 0-0-dbConfig.php
@@ -0,0 +1,7 @@
+<?php
+        $serverName = "localhost";
+        $IPserver = "127.0.1";
+        $username = "applicazioneWeb";
+        $password = "123456_Web&&";
+        $db = "listaeventi";
+?>
